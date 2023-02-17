d1d7f1fab688ffa46f0a10a7e5f3871981cd92db
Joel Denning <joeldenning@gmail.com>
2023-02-17 14:47:57 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@
 # may cause git to complain with `detected dubious ownership in repository`.
 # mark everything as safe here as we don't know where the repository is (it
 # might be something other than $GITHUB_WORKSPACE)
+# see https://github.com/actions/runner-images/issues/6775
+# see https://github.com/crytic/slither-action/pull/50/files
 git config --global --add safe.directory '*'
 
 command="eb $1"
