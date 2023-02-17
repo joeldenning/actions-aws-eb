a7816f7a99359fab862fb52ed432e889ebc5645c
joeldenning <joeldenning@gmail.com>
2023-02-17 14:43:07 -0700

    Try fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+# the user/group info may mismatch between container and host, and it
+# may cause git to complain with `detected dubious ownership in repository`.
+# mark everything as safe here as we don't know where the repository is (it
+# might be something other than $GITHUB_WORKSPACE)
+git config --global --add safe.directory '*'
+
 command="eb $1"
 
 $command
